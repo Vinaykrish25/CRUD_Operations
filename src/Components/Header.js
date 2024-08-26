33de9251288269a2dd3908b8a7cac7307e0259de
Vinaykrish25 <vinaykrish2002@gmail.com>
2024-08-26 22:25:01 +0530

    updated

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,12 +2,13 @@ import React, { useContext, useState } from 'react'
 import "./Styles/Header.css"
 import { ThemeContext } from '../Context/ThemeContext'
 import logo from "../Images/logo.png"
+import {Link} from "react-router-dom"
 
 const Header = () => {
     const {headercolor, changetheme} = useContext(ThemeContext);
     return (
         <div className='header-container' style={{backgroundColor:headercolor}}>
-            <img src={logo} alt="tesla" width={100} height={50}/>
+            <Link to="/"><img src={logo} alt="tesla" width={100} height={50}/></Link>
             <label class="switch">
                 <input type="checkbox" onClick={changetheme}/>
                     <span class="slider round"></span>
